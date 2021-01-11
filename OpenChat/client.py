64563c2f368214150c58fa0dd01c721997c5f5d6
User-92 <brayden@mobilebayarea.com>
2021-01-10 19:07:14 -0600

    Chat window added.

--- OpenChat/client.py
+++ OpenChat/client.py
@@ -1,4 +1,7 @@
-import socket, select
+import socket, select, time, sys, pygame
+clock = pygame.time.Clock()
+from pygame.locals import *
+pygame.init()
 
 def get_ip_port(path):
     with open(path, "r") as ipf:
@@ -19,8 +22,23 @@ class Network():
         except socket.error as e:
             print(e)
             
+    def send(self, data):
+        try:
+            self.socket.sendall((f"{len(data):<{HEADER_SIZE}}"+data).encode())
+        except Exception as e:
+            print(f"Error: {e}")
+            
     def disconnect(self):
         self.socket.close()
+        
+pygame.display.set_caption("OpenChat")
+screen = pygame.display.set_mode((500,500))
+
+# FONTS
+title_font = pygame.font.SysFont(None, 36)
+title_text = title_font.render('OpenChat Testing Chat Room', True, (255,255,255))
+title_rect = pygame.Rect(0,0,500,70)
+message_font = pygame.font.SysFont(None, 16)
         
 n = Network()
 n.connect()
@@ -28,7 +46,22 @@ n.connect()
 BUFFER_SIZE = 2048
 HEADER_SIZE = 5
 
+messages = []
+full_message = ""
+enter_message_text = message_font.render("Enter A Message --> ", True, (255,255,255))
+
 while True:
+    screen.fill((0,0,0))
+    
+    pygame.draw.rect(screen, (60,60,60), title_rect)
+    screen.blit(title_text, (70, 20))
+    screen.blit(enter_message_text, (10, 474))
+    
+    m_offset = 0
+    for message in messages:
+        screen.blit(message, (10,450-m_offset))
+        m_offset += 22
+    
     ins, outs, ex = select.select([n.socket], [], [], 0)
     for in_ in ins:
         data = ""
@@ -52,5 +85,26 @@ while True:
             data += recv_data.decode()
         data = data[HEADER_SIZE:message_length+HEADER_SIZE]
         
-        print(data)
+        messages.insert(0, message_font.render(data, True, (255,255,255)))
+            
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == TEXTINPUT:
+            full_message += event.text
+            enter_message_text = message_font.render(f"Enter A Message --> {full_message}", True, (255,255,255))
+        elif event.type == KEYDOWN:
+            if event.key == K_RETURN:
+                n.send(full_message)
+                full_message = ""
+                enter_message_text = message_font.render("Enter A Message --> ", True, (255,255,255))
+            if event.key == K_BACKSPACE:
+                full_message = full_message[:-1]
+                enter_message_text = message_font.render(f"Enter A Message --> {full_message}", True, (255,255,255))
+            
+    pygame.display.update()
+    clock.tick(60)
+        
+    
 n.disconnect()
--- OpenChat/server.py
+++ OpenChat/server.py
@@ -6,6 +6,12 @@ def get_ip_port(path):
         ipf.close()
     data_split = ipf_data.split(":")
     return [data_split[0],int(data_split[1])]
+    
+def send_data(data, conn):
+    try:
+        conn.sendall((f"{len(data):<{HEADER_SIZE}}"+data).encode())
+    except Exception as e:
+        print(f"Error: {e}")
 
 ipport = get_ip_port("server-ip.txt")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,47 +21,55 @@ bufsize = 2048
 
 HEADER_SIZE = 5
 
-def client_thread(conn):
-    try:
-        #print(json.dumps(update).encode())
-        send_data = "#####################################\nWelcome To OpenChat's Testing Server!\n#####################################\n"
-        conn.sendall((f"{len(send_data):<{HEADER_SIZE}}"+send_data).encode())
-    except Exception as e:
-        print(f"Error: {e}")
-    conn.close()
-        #try:
-        #    data = ""
-        #    recv_data = in_.recv(BUFFER_SIZE)
-        #    message_length = int(recv_data[:HEADER_SIZE])
-        #    data += recv_data.decode()
-        #    while True:
-        #        if len(data)-HEADER_SIZE >= message_length:
-        #            if len(data)-HEADER_SIZE > message_length:
-        #                n.socket.setblocking(0)
-        #                while True:
-        #                    try:
-        #                        extra_data = in_.recv(BUFFER_SIZE)
-        #                    except:
-        #                        break
-        #                n.socket.setblocking(1)
-        #            break
-        #        recv_data = in_.recv(BUFFER_SIZE)
-        #        data += recv_data.decode()
-        #    data = data[HEADER_SIZE:message_length+HEADER_SIZE]
-        #    
-        #    if data:
-        #        print(data)
-        #        
-        #except Exception as e:
-        #    print(e)
-        #    break
+def client_thread(conn,users):
+    send_data("#####################################\nWelcome To OpenChat's Testing Server!\n#####################################\n", conn)
         
+    while True:
+        try:
+            data = ""
+            recv_data = conn.recv(bufsize)
+            if recv_data == b'':
+                print("Someone disconnected")
+                break
+            message_length = int(recv_data[:HEADER_SIZE])
+            data += recv_data.decode()
+            while True:
+                if len(data)-HEADER_SIZE >= message_length:
+                    if len(data)-HEADER_SIZE > message_length:
+                        n.socket.setblocking(0)
+                        while True:
+                            try:
+                                extra_data = conn.recv(bufsize)
+                            except:
+                                break
+                        n.socket.setblocking(1)
+                    break
+                recv_data = conn.recv(bufsize)
+                data += recv_data.decode()
+            data = data[HEADER_SIZE:message_length+HEADER_SIZE]
+            
+            if data:
+                for user in users:
+                    if user == conn:
+                        send_data("YOU > "+data,user)
+                    else:
+                        send_data("UnknownUser > "+data,user)
+                print(data)
+                
+        except Exception as e:
+            print(e)
+            break
+            
+    users.remove(conn)
+    conn.close()
 
 print("Server has started, waiting for connections.")
+users = []
 
 while True:
     conn, addr = s.accept()
     print(f"Connection from {addr}")
+    users.append(conn)
     
-    c_thread = threading.Thread(target=client_thread, args=[conn])
+    c_thread = threading.Thread(target=client_thread, args=[conn,users])
     c_thread.start()
